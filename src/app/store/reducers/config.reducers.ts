9b6aeb36f4a0fdebaf5fe7a326b857c3d5715159
Phalguna kumar <pkrachoor@gmail.com>
2018-12-21 16:10:22 -0500

    added the config reducers

---
+++ src/app/store/reducers/config.reducers.ts
@@ -0,0 +1,17 @@
+import { ConfigActions, EConfigActions } from './../actions/config.actions';
+import { initialConfigState, IConfigState } from './../state/config.state';
+export const ConfigReducers = (
+  state = initialConfigState,
+  action: ConfigActions
+) => {
+  switch (action.type) {
+    case EConfigActions.GetConfig: {
+      return {
+        ...state,
+        config: action.type,
+      };
+    }
+    default:
+      return state;
+  }
+};
